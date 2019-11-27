51054ba2df540cd5a7b6b58d734ec521680e3ccb
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-27 15:40:59 -0700

    Finished eel, ferret, groundhog, and horse in initialize

---
+++ initialize/lib/ferret.rb
@@ -0,0 +1,12 @@
+class Ferret
+
+  attr_reader :name
+
+  def initialize(name = "a ferret has no name")
+    @name = name
+  end
+
+  def give_name(name)
+    @name = "a ferret's name is #{name}"
+  end
+end
