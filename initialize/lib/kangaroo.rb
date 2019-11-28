8c64cb2679e824cca66efc4c40aff0d2145505b0
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-27 18:22:13 -0700

    Finished kangaroo and lion in initialize

---
+++ initialize/lib/kangaroo.rb
@@ -0,0 +1,10 @@
+class Kangaroo
+
+  attr_reader :name, :children
+
+  def initialize(name, children)
+    @name = name
+    @children = children
+  end
+
+end
---
+++ initialize/lib/lion.rb
@@ -0,0 +1,10 @@
+class Lion
+
+  attr_reader :name, :sound
+
+  def initialize(args)
+    @name = args[:name]
+    @sound = args[:sound]
+  end
+
+end
