d9c548c3bcfc7143adb9cd72e13007f98ff3c524
Meghan Stovall <meghan.stovall@gmail.com>
2019-11-30 21:06:53 -0700

    Finished ints and floats in data types

--- data-types/ints_and_floats/ints_and_floats.rb
+++ data-types/ints_and_floats/ints_and_floats.rb
@@ -7,86 +7,67 @@ class IntsAndFloatsTests < Minitest::Test
     unlucky = 13
     # Using the two variables defined above,
     # add the lucky number and the unlucky number
-    sum = ________
-    assert_equal 20, sum
+    assert_equal 20, lucky + unlucky
   end
 
   def test_2
-    skip
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # subtract the unlucky from the lucky
-    difference = ________
-    assert_equal -6, difference
+    assert_equal -6, lucky - unlucky
   end
 
   def test_3
-    skip
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
     # NOTE: this is integer division
-    quotient = ________
-    assert_equal 1, quotient
+    assert_equal 1, unlucky / lucky
   end
 
   def test_4
-    skip
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
-    quotient = ________
-    assert_equal 1.8571428571428572, quotient
+    assert_equal 1.8571428571428572, unlucky.to_f / lucky.to_f
   end
 
   def test_5
-    skip
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # find the remainder of the unlucky divided by the lucky
-    remainder = ____________
-    assert_equal 6, remainder
+    assert_equal 6, unlucky % lucky
   end
 
   def test_6
-    skip
     lucky = 7
     # Using the variable defined above,
     # find out if the lucky number is even
-    even = _________
-    assert_equal false, even
+    assert_equal false, lucky.even?
   end
 
   def test_7
-    skip
     pi = 3.14
     # Using the variable defined above,
     # round the number to the nearest whole number
-    rounded = _________
-    assert_equal 3, rounded
+    assert_equal 3, pi.to_i
   end
 
   def test_8
-    skip
     pi = 3.14
     # Using the variable defined above,
     # round the number to one decimal place
-    rounded = _________
-    assert_equal 3.1, rounded
+    assert_equal 3.1, pi.round(1)
   end
 
-
-
   def test_9
-    skip
     pi = 3.14
     # Using the variable defined above,
     # round the number to the next highest whole number
-    rounded = _________
-    assert_equal 4, rounded
+    assert_equal 4, pi.ceil
   end
 end
