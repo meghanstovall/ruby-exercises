0e046b84e89fbd7d81a8910f2c8fc800ae7bbbfa
Meghan Stovall <meghan.stovall@gmail.com>
2019-12-09 21:02:19 -0700

    Finished find_pattern_test in enumerables/exercise_1

--- enumerables/exercises_1/find_pattern_test.rb
+++ enumerables/exercises_1/find_pattern_test.rb
@@ -20,65 +20,101 @@ class FindPatternTest < Minitest::Test
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
     found = nil
     words.each do |word|
-      # Your code goes here
+      if word == "waldo"
+        found = "I found waldo!"
+      end
     end
     assert_equal nil, found
   end
 
   def test_find_waldo
-    skip
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
     found = nil
-    # Your code goes here
+    words.each do |word|
+      if word == "waldo"
+        found = "waldo"
+      end
+    end
     assert_equal "waldo", found
   end
 
   def test_cannot_find_3_letter_words
-    skip
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    found = nil
+    words.each do |word|
+      if word.length == 3
+        found = word
+      end
+    end
     assert_equal nil, found
   end
 
   def test_find_13
-    skip
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = 0
+    numbers.each do |number|
+      if number == 13
+        found = number
+      end
+    end
     assert_equal 13, found
   end
 
   def test_find_first_even_number
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = 0
+    numbers.each do |number|
+      if number.even?
+        return number
+      end
+    end
     assert_equal 10, found
   end
 
   def test_find_first_multiple_of_3
-    skip
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = 0
+    numbers.each do |number|
+      if number % 3 == 0
+        return number
+      end
+    end
     assert_equal 9, found
   end
 
   def test_find_first_word_starting_with_q
-    skip
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+    found = ""
+    words.each do |word|
+      if word[0] == "q"
+        found = word
+        return found
+      end
+    end
     assert_equal "quill", found
   end
 
   def test_find_first_word_ending_with_er
-    skip
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
+    found = ""
+    words.each do |word|
+      if word.end_with?("er")
+        found = word
+        return found
+      end
+    end
     assert_equal "finger", found
   end
 
   def test_find_first_number_greater_than_20
-    skip
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = 0
+    numbers.each do |number|
+      if number > 20
+        found = number
+        return found
+      end
+    end
     assert_equal 21, found
   end
 
