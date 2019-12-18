9d00fd3fe4d5d987c21f0f2b8cb5d22327401652
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-18 11:02:05 -0700

    Finish baby_test in command-query/exercises

--- command-query/exercises/baby_test.rb
+++ command-query/exercises/baby_test.rb
@@ -10,10 +10,8 @@ class BabyTest < Minitest::Test
   end
 
   def test_not_so_tired_after_a_nap
-    skip
     baby = Baby.new
     baby.nap
     refute baby.tired?, "Naps help combat drowsiness."
   end
-
 end
