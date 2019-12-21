1b9525b55838bbe8a84ec18984d65bb187910bf6
Meghan Stovall <meghanstovall15@augustana.edu>
2019-12-20 21:12:37 -0700

    Finish group_by_pattern_test in enumerables/exercises_1

--- enumerables/exercises_1/group_by_pattern_test.rb
+++ enumerables/exercises_1/group_by_pattern_test.rb
@@ -18,41 +18,69 @@ class GroupByPatternTest < Minitest::Test
     numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     odd_and_even = Hash.new {|hash, key| hash[key] = []}
     numbers.each do |number|
-      # Your code goes here
+      odd_and_even[1] << number if number.odd?
+      odd_and_even[0] << number if number.even?
     end
     expected = {1=>[1, 1, 3, 5, 13, 21, 55], 0=>[2, 8, 34]}
     assert_equal expected, odd_and_even
   end
 
   def test_group_words_by_first_letter
-    skip
     words = ["ant", "axis", "albatross", "bolt", "badge", "butter", "car", "cdr", "column"]
     words_by_first_letter = Hash.new {|hash, key| hash[key] = []}
-    # Your code goes here
+    words.each do |word|
+      words_by_first_letter['a'] << word if word[0] == 'a'
+      words_by_first_letter['b'] << word if word[0] == 'b'
+      words_by_first_letter['c'] << word if word[0] == 'c'
+    end
+
     expected = {"a"=>["ant", "axis", "albatross"], "b"=>["bolt", "badge", "butter"], "c"=>["car", "cdr", "column"]}
     assert_equal expected, words_by_first_letter
   end
 
   def test_group_words_by_uniqueness
-    skip
     words = ["one", "two", "one", "TWO", "three", "one", "three", "three", "three"]
-    # Your code goes here
+    grouped = Hash.new {|hash, key| hash[key] = []}
+    words.each do |word|
+      grouped["one"] << word if word.downcase == "one"
+      grouped["two"] << word if word.downcase == "two"
+      grouped["three"] << word if word.downcase == "three"
+    end
+
     expected = {"one"=>["one", "one", "one"], "two"=>["two", "TWO"], "three"=>["three", "three", "three", "three"]}
     assert_equal expected, grouped
   end
 
   def test_group_by_number_of_zeros
-    skip
     numbers = [1, 3, 500, 200, 4000, 3000, 10000, 90, 20, 500000]
-    # Your code goes here
+    grouped = Hash.new {|hash, key| hash[key] = []}
+    numbers.each do |number|
+      count = 0
+      num_array = number.to_s.split("")
+      num_array.each do |num|
+        if num.to_i == 0
+          count += 1
+        end
+      end
+      grouped[count] << number
+    end
+
     expected = {0=>[1, 3], 2=>[500, 200], 3=>[4000, 3000], 4=>[10000], 1=>[90, 20], 5=>[500000]}
     assert_equal expected, grouped
   end
 
   def test_group_by_order_of_magnitude
-    skip
     numbers = [1, 3, 503, 239, 4938, 3932, 19982, 93, 21, 501787]
-    # Your code goes here
+    grouped = Hash.new {|hash, key| hash[key] = []}
+    numbers.each do |number|
+      count = 0
+      num = number.to_s
+      if num.length > 0
+        count += num.length
+        grouped[count] << number
+      end
+    end
+
     expected = {1=>[1, 3], 2=>[93, 21], 3=>[503, 239], 4=>[4938, 3932], 5=>[19982], 6=>[501787]}
     assert_equal expected, grouped
   end
